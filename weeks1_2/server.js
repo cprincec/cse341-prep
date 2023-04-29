bd34028877aac4ac63b34a762a9c57d4dcd57a77
cprincec <chukwuprincec@gmail.com>
2023-04-29 17:01:18 +0100

    uncommented morgan and helmet

--- weeks1_2/server.js
+++ weeks1_2/server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
-// const morgan = require('morgan'); // This is used for logging http requests
-// const helmet = require("helmet"); // This is a security middleware that protects our app from attackers.
+const morgan = require('morgan'); // This is used for logging http requests
+const helmet = require("helmet"); // This is a security middleware that protects our app from attackers.
 require('dotenv').config();
 
 
@@ -11,8 +11,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(morgan('common')); // morgan middleware using the 'common'  logging format;
-// app.use(helmet());
+app.use(morgan('common')); // morgan middleware using the 'common'  logging format;
+app.use(helmet());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
