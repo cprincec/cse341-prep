a4cab987b0c8dfb6d0da754c8fa95580e62be7e6
cprincec <chukwuprincec@gmail.com>
2023-05-09 15:59:20 +0100

    setting up ecommerce api

---
+++ weeks3_4/project/src/app.js
@@ -0,0 +1,26 @@
+const app = require("express")();
+const mongodb = require("./db/connect");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
+
+mongodb
+  .main()
+  .catch(console.error)
+  .then(() =>
+    app.listen(
+      PORT,
+      console.log(
+        "DB connected and server started on: " + PORT
+      )
+    )
+  );
---
+++ weeks3_4/project/src/db/connect.js
@@ -0,0 +1,46 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+// variable to store database connection
+let _db;
+
+// function to connect database
+async function main() {
+    // database connection string
+    const uri = process.env.URI;
+
+    // create instance of mongoclient wi
+    let client = new MongoClient(uri);
+
+    try {
+        // connect to database
+        await client.connect();
+        await listDatabases(client)
+
+        // store database connection in _db variable
+        _db = client;
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function getDb() {
+    if (!_db) {
+        throw Error("Database not connected.")
+    } else {
+        return _db
+    }
+}
+
+// function to print list of databases in a cluster
+async function listDatabases(client) {
+    let databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:\n");
+    databasesList.databases.forEach(db => { console.log(`-${db.name}`);});
+    console.log("\n");
+}
+
+
+module.exports = { main, getDb };
