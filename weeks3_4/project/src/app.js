3f424021dffcf05673ffa8fc831906f5e9f0cd6a
cprincec <chukwuprincec@gmail.com>
2023-08-18 20:21:50 +0100

    added method

--- weeks3_4/project/src/app.js
+++ weeks3_4/project/src/app.js
@@ -26,7 +26,7 @@ app
     if (allowedOrigins.includes(origin)) {
          res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
