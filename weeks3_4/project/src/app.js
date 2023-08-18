1aa24577c6dbfa828b3fa12babc8eda12c2f2a96
cprincec <chukwuprincec@gmail.com>
2023-08-18 20:20:51 +0100

    headers

--- weeks3_4/project/src/app.js
+++ weeks3_4/project/src/app.js
@@ -20,21 +20,34 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     const allowedOrigins = [
-      "http://localhost:5173",
-      "http://192.168.55.198:5173",
-    ];
-
-    const origin = req.headers.origin;
+          "http://localhost:5173",
+          "http://192.168.55.198:5173",
+        ]; const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
     return next();
-  })
+  });
+  // .use((req, res, next) => {
+  //   const allowedOrigins = [
+  //     "http://localhost:5173",
+  //     "http://192.168.55.198:5173",
+  //   ];
+
+  //   const origin = req.headers.origin;
+  //   if (allowedOrigins.includes(origin)) {
+  //     res.setHeader("Access-Control-Allow-Origin", origin);
+  //   }
+  //   res.setHeader("Access-Control-Allow-Credentials", true);
+  //   res.setHeader(
+  //     "Access-Control-Allow-Headers",
+  //     "Content-Type, Authorization"
+  //   );
+  //   return next();
+  // })
 
   // Sessions
   .use(
