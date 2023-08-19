cec8de985dac7561cc2605eb54c852281492c5c7
cprincec <chukwuprincec@gmail.com>
2023-08-19 01:36:18 +0100

    test

--- weeks3_4/project/src/app.js
+++ weeks3_4/project/src/app.js
@@ -69,6 +69,7 @@ app
     session({
       secret: process.env.SESSION_SECRET,
       resave: false,
+      name: "test",
       // don't save a session until something is saved
       saveUninitialized: false,
       cookie: { secure: true, httpOnly: false, maxAge: 1000 * 72 * 60 },
