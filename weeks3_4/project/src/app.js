7cba2caee1ff048d53e68e805fea842ab960d7c3
cprincec <chukwuprincec@gmail.com>
2023-08-19 01:54:53 +0100

    testing

--- weeks3_4/project/src/app.js
+++ weeks3_4/project/src/app.js
@@ -18,34 +18,22 @@ app
   .use(helmet())
   .use(express.json())
   .use(bodyParser.json())
+  .use(cors())
   .use((req, res, next) => {
     const allowedOrigins = [
       "http://localhost:5173",
       "http://192.168.55.198:5173",
       "https://centralmall.netlify.app",
     ];
-    const requestOrigin = req.headers.origin;
-    let origin;
-    if (allowedOrigins.includes(requestOrigin)) {
-      origin = requestOrigin;
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
     }
-    cors({ credentials: true, origin: origin });
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+    return next();
   })
-  // .use((req, res, next) => {
-  //   const allowedOrigins = [
-  //     "http://localhost:5173",
-  //     "http://192.168.55.198:5173",
-  //     "https://centralmall.netlify.app",
-  //   ];
-  //   const origin = req.headers.origin;
-  //   if (allowedOrigins.includes(origin)) {
-  //     res.setHeader("Access-Control-Allow-Origin", origin);
-  //   }
-  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  //   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  //   res.header("Access-Control-Allow-Credentials", true);
-  //   return next();
-  // })
   // .use((req, res, next) => {
   //   const allowedOrigins = [
   //     "http://localhost:5173",
