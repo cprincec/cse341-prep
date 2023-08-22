f7e2c595df4e17b546a1408fd2505dbd33d1b39d
cprincec <chukwuprincec@gmail.com>
2023-08-22 06:55:04 +0100

    setHeaders

--- weeks3_4/project/src/app.js
+++ weeks3_4/project/src/app.js
@@ -18,6 +18,18 @@ app
   .use(helmet())
   .use(express.json())
   .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 
   //   .use((req, res, next) => {
   //     console.log("app");
