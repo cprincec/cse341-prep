3e7f33dd54bbeb3a83c9b0d33ca9245a3f767699
cprincec <chukwuprincec@gmail.com>
2023-08-24 12:42:39 +0100

    fix

--- weeks3_4/project/src/model/Cart.js
+++ weeks3_4/project/src/model/Cart.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema({
-    productId: {
-        type: String,
-        trim: true,
-        required: [true, ]
-    }
-
-})
+  productId: {
+    type: String,
+    trim: true,
+    required: [true],
+  },
+});
